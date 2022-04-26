46767a3bdcc642bf100c5c6906cee9b729c80eb7
Batur <fairdose@cyberservices.com>
2022-04-26 20:03:15 +0300

    General improvements - Component name change - Style improvements

---
+++ src/lib/layout/Sidebar.svelte
@@ -0,0 +1,19 @@
+<script>
+
+</script>
+
+<div class="sidebar">
+    I am head
+</div>
+
+<style lang="scss">
+  .sidebar {
+    position: absolute;
+    left: 0;
+    top: 0;
+    background-color: black;
+    height: 100vh;
+    min-width: calc(320px - 1em);
+    padding: 1em;
+  }
+</style>
